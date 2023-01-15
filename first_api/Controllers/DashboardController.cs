f7d46c13c0bd3b703491176bc593a91216c5b753
NAMRATA SHAH <78063600+namrata-rgb@users.noreply.github.com>
2023-01-15 23:06:18 +0530

    Add files via upload

---
+++ first_api/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace first.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult report()
+        {
+            return View();
+        }
+    }
+}
